2154653af4eb48cd1f59c0359129f333e78ba9e9
DamoFD <voshall@yahoo.com>
2024-08-13 00:14:39 -0700

    fixed input modal bug

--- frontend/src/editor/components/InputModal.jsx
+++ frontend/src/editor/components/InputModal.jsx
@@ -66,7 +66,7 @@ const InputModal = ({isOpen, closeModal, modalName, fields, suggestions}) => {
                         <div className="card-depth bg-red-400 p-2 cursor-pointer text-brand-black" onClick={handleDeleteField}>
                             <svg className="size-8" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zM8 9h8v10H8zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
                         </div>
-                        <button className="card-depth bg-green-400 p-2 cursor-pointer text-brand-black font-hasken font-bold text-lg" onClick={closeModal}>Submit</button>
+                        <button className="card-depth bg-green-400 p-2 cursor-pointer text-brand-black font-hasken font-bold text-lg" onClick={handleCloseModal}>Submit</button>
                     </div>
             </div>
         </div>
