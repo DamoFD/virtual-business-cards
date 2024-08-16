06bc32c15d2ec1880c2225d1d1b511a24f53a6e6
DamoFD <voshall@yahoo.com>
2024-08-15 20:58:19 -0700

    db comments

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,3 +1,7 @@
+/*
+    Package db contains database configurations and methods.
+    It contains a struct PostgresConfig and a method NewPostgreSQLStorage() that initializes the database.
+*/
 package db
 
 import (
@@ -8,15 +12,19 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// PostgresConfig contains configurations for the PostgreSQL database.
 type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
-	SSLMode  string
+	Host     string // DB_HOST
+	Port     string // DB_PORT
+	User     string // DB_USER
+	Password string // DB_PASSWORD
+	DBName   string // DB_NAME
+	SSLMode  string // DB_SSL_MODE
 }
 
+// FormatURL formats the URL for the PostgreSQL database.
+// It takes a PostgresConfig struct as an argument.
+// It returns the formatted URL.
 func (cfg PostgresConfig) FormatURL() string {
 	return fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
@@ -29,6 +37,9 @@ func (cfg PostgresConfig) FormatURL() string {
 	)
 }
 
+// NewPostgreSQLStorage initializes the database.
+// It takes a PostgresConfig struct as an argument.
+// It returns a *sql.DB and an error if the database fails to initialize.
 func NewPostgreSQLStorage(cfg PostgresConfig) (*sql.DB, error) {
 	url := cfg.FormatURL()
 	db, err := sql.Open("postgres", url)
