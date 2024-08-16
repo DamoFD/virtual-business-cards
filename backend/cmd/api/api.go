73eb1444eddd32e04a6cb1cca3da1f1364bcd69d
DamoFD <voshall@yahoo.com>
2024-08-15 20:51:43 -0700

    api comments

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -1,3 +1,13 @@
+/*
+    Package api contains API server configurations and methods.
+    It contains a struct APIServer and a method Run() that starts the API server.
+
+    Example:
+        server := api.NewAPIServer(":8080", db)
+        if err := server.Run(); err != nil {
+            log.Fatal(err)
+        }
+*/
 package api
 
 import (
@@ -8,11 +18,15 @@ import (
     "github.com/gorilla/mux"
 )
 
+// APIServer contains servers configurations.
 type APIServer struct {
-	addr string
-	db   *sql.DB
+	addr string // addr is the address of the server.
+	db   *sql.DB // db is the database connection.
 }
 
+// NewAPIServer creates a new APIServer instance.
+// It takes an address and a database connection as arguments.
+// It returns an APIServer instance.
 func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	return &APIServer{
 		addr: addr,
@@ -20,6 +34,9 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	}
 }
 
+// Run starts the API server.
+// It returns an error if the server fails to start.
+// It returns nil if the server starts successfully.
 func (s *APIServer) Run() error {
     router := mux.NewRouter()
     // subrouter := router.PathPrefix("/api/v1").Subrouter()
